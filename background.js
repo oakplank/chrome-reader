ed46941ee070949f5761451e8dfa1893f50de20a
oakplank <jacob.branch44@gmail.com>
2024-11-24 15:40:42 -0800

    WIP added pdfjs to enable pdf reading

--- background.js
+++ background.js
@@ -1,25 +1,26 @@
 // Listen for keyboard shortcut
 chrome.commands.onCommand.addListener((command) => {
-    if (command === "start-speed-reading") {
-      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+  if (command === "start-speed-reading") {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      if (tabs[0].id) {
         chrome.tabs.sendMessage(tabs[0].id, { action: "start-speed-reading" });
-      });
-    }
-  });
-  
-  // Create context menu item (optional)
-  chrome.runtime.onInstalled.addListener(() => {
-    chrome.contextMenus.create({
-      id: "start-speed-reading",
-      title: "Start Speed Reading",
-      contexts: ["selection"]
+      }
     });
+  }
+});
+
+// Create context menu item (optional)
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: "start-speed-reading",
+    title: "Start Speed Reading",
+    contexts: ["selection"]
   });
-  
-  // Handle context menu click
-  chrome.contextMenus.onClicked.addListener((info, tab) => {
-    if (info.menuItemId === "start-speed-reading") {
-      chrome.tabs.sendMessage(tab.id, { action: "start-speed-reading", selectedText: info.selectionText });
-    }
-  });
-  
+});
+
+// Handle context menu click
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === "start-speed-reading" && info.selectionText) {
+    chrome.tabs.sendMessage(tab.id, { action: "start-speed-reading", selectedText: info.selectionText });
+  }
+});
